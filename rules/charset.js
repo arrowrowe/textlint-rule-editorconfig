ced0b6a42cacc71a79d66d68a6ae67f4d9edd641
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2023-07-08 00:28:50 +0200

    HACK: ASCII is a subset of UTF-8, so we don't need to report it
    See https://github.com/CharsetDetector/UTF-unknown/issues/161

--- rules/charset.js
+++ rules/charset.js
@@ -13,14 +13,24 @@ const rule = (config, args) => {
   const expected = config.charset.toLowerCase();
   const real = jschardet.detect(args.text);
   real.encoding = real.encoding.toLowerCase();
-  if (real.encoding !== expected) {
-    utilReport(
-      args,
-      {index: 0},
-      'Found ' + real.confidence.toString() + ' possible wrong charset "'
-        + real.encoding + '", expected "' + expected + '".',
-    );
+
+  if (real.encoding === expected) {
+    return;
   }
+
+  // HACK: ASCII is a subset of UTF-8, so we don't need to report it.
+  //       See https://github.com/CharsetDetector/UTF-unknown/issues/161
+  // eslint-disable-next-line unicorn/text-encoding-identifier-case
+  if (real.encoding === 'ascii' && expected === 'utf-8') {
+    return;
+  }
+
+  utilReport(
+    args,
+    {index: 0},
+    'Found ' + real.confidence.toString() + ' possible wrong charset "'
+      + real.encoding + '", expected "' + expected + '".',
+  );
 };
 
 export default rule;
